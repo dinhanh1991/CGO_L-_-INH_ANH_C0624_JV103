7c878cb640a8c4435345eb34ae438d0b1051da8f
Your Name <you@example.com>
2024-09-23 22:57:45 +0700

    nopbaiadd_excercise

--- module2/src/add_excercise/management_traffic_vihicle/Model/Manufacture.java
+++ module2/src/add_excercise/management_traffic_vihicle/Model/Manufacture.java
@@ -13,6 +13,31 @@ public class Manufacture {
         this.name = name;
         this.country = country;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public String toString() {
         return name + " (" + country + ")";
     }
--- module2/src/add_excercise/management_traffic_vihicle/service/VehicleManagementService.java
+++ module2/src/add_excercise/management_traffic_vihicle/service/VehicleManagementService.java
@@ -4,17 +4,40 @@ import add_excercise.management_traffic_vihicle.Model.*;
 import ss5.thuc_tap.static_property.Car;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class VehicleManagementService {
     private ArrayList<Vehicles> vehicles = new ArrayList<>();
-    private static ArrayList<Manufacture> manufactures = new ArrayList<>();
+    private  ArrayList<Manufacture> motorbikeManufactures = new ArrayList<>();
+    private  ArrayList<Manufacture> truckManufactures = new ArrayList<>();
+    private  ArrayList<Manufacture> carManufactures = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in);
+    private  Random random =new Random();
 
-    static {
-        manufactures.add(new Manufacture("A01", "Toyota", "Japan"));
-        manufactures.add(new Manufacture("A02", "Vinfast", "Viet Nam"));
-        manufactures.add(new Manufacture("A01", "Kia", "Sount Korea"));
+    public VehicleManagementService() {
+        motorbikeManufactures.add(new Manufacture("01","Honda","Japan"));
+        motorbikeManufactures.add(new Manufacture("02","Yamaha","Japan"));
+        motorbikeManufactures.add(new Manufacture("03","Suzuki","Japan"));
+        motorbikeManufactures.add(new Manufacture("04","Piaggio","Italy"));
+        motorbikeManufactures.add(new Manufacture("05","Harley-Davidson","USA"));
+        motorbikeManufactures.add(new Manufacture("06","Ducati","Italia"));
+        carManufactures.add(new Manufacture("01","Honda","Japan"));
+        carManufactures.add(new Manufacture("02","Toyota","Japan"));
+        carManufactures.add(new Manufacture("03","Volkswagen","Germany"));
+        carManufactures.add(new Manufacture("04","BMW","Germany"));
+        carManufactures.add(new Manufacture("05","Hyundai","Korea"));
+        carManufactures.add(new Manufacture("06","Mazda","Japan"));
+        carManufactures.add(new Manufacture("07","Audi","Germany"));
+        carManufactures.add(new Manufacture("08","Porsche","Germany"));
+        truckManufactures.add(new Manufacture("01","Fuso","Japan"));
+        truckManufactures.add(new Manufacture("02","Dongfeng","China"));
+        truckManufactures.add(new Manufacture("03","DaeWoo","India"));
+        truckManufactures.add(new Manufacture("04","Hyundai","Korea"));
+        truckManufactures.add(new Manufacture("05","Trường Hải Auto","Viet Nam"));
+        truckManufactures.add(new Manufacture("06","Isuzu","Japan"));
+        truckManufactures.add(new Manufacture("07","Jac","China"));
+
     }
 
     public void addVehicle() {
@@ -22,8 +45,6 @@ public class VehicleManagementService {
 
             System.out.println("Enter number plate");
             String numberPlate = scanner.nextLine();
-            System.out.println("Enter vehicle manufacturer");
-            String manufacturer = scanner.nextLine();
             System.out.println("Enter owner's vehicle");
             String ownerName = scanner.nextLine();
             System.out.println("Enter year of manufacture's vehicle");
@@ -40,9 +61,16 @@ public class VehicleManagementService {
                     System.out.println("Add truck");
                     System.out.println("Enter load capacity (unit: Ton)");
                     int loadCapacity = Integer.parseInt(scanner.nextLine());
-                    vehicles.add(new Trucks(numberPlate, manufacturer,
+                    String truckManufacturer =truckManufactures.get(random.nextInt(truckManufactures.size())).getName();
+                    vehicles.add(new Trucks(numberPlate, truckManufacturer,
                             yearOfManufacture, ownerName, loadCapacity));
                     System.out.println("Add success");
+                    System.out.println("The new Vehicle just added: ");
+                    for (Vehicles vehicle : vehicles) {
+                        if (vehicle instanceof Trucks) {
+                            System.out.println(vehicle.toString());
+                        }
+                    }
                     break;
                 case 2:
                     System.out.println("Add Car");
@@ -50,17 +78,29 @@ public class VehicleManagementService {
                     int seat = Integer.parseInt(scanner.nextLine());
                     System.out.println("Enter type of car");
                     String typeOfCar = scanner.nextLine();
-                    vehicles.add(new Cars(numberPlate, manufacturer,
+                    String carManufacturer =carManufactures.get(random.nextInt(truckManufactures.size())).getName();
+                    vehicles.add(new Cars(numberPlate, carManufacturer,
                             yearOfManufacture, ownerName, seat, typeOfCar));
                     System.out.println("Add success");
+                    for (Vehicles vehicle : vehicles) {
+                        if (vehicle instanceof Cars) {
+                            System.out.println(vehicle.toString());
+                        }
+                    }
                     break;
                 case 3:
                     System.out.println("Add Motorbike");
                     System.out.println("Enter engine power (unit: HP)");
                     int enginePower = Integer.parseInt(scanner.nextLine());
-                    vehicles.add(new Motorbikes(numberPlate, manufacturer
+                    String motorbikeManufacturer =motorbikeManufactures.get(random.nextInt(truckManufactures.size())).getName();
+                    vehicles.add(new Motorbikes(numberPlate, motorbikeManufacturer
                             , yearOfManufacture, ownerName, enginePower));
                     System.out.println("Add success");
+                    for (Vehicles vehicle : vehicles) {
+                        if (vehicle instanceof Motorbikes) {
+                            System.out.println(vehicle.toString());
+                        }
+                    }
                     break;
                     case 0:
                         System.out.println("Exit");
@@ -99,10 +139,12 @@ public class VehicleManagementService {
             vehicles.removeIf(vehicle -> vehicle.getNumberPlate().equals(numberPlate));
             System.out.println("Delete success");
         }else {
-            System.out.println("There are many vehicles which are greater than one,do you wana delete :");
+            System.out.println("There are many vehicles which are greater than one,do you wana delete :\n" +
+                    "input true to agree\n" +
+                    "input false to disagree");
             boolean chosen = scanner.nextBoolean();
             if (chosen) {
-                System.out.println("you say yes delete...");
+                System.out.println("you agreed to delete...");
                 vehicles.removeIf(vehicle -> vehicle.getNumberPlate().equals(numberPlate));
                 System.out.println("successfully deleted");
                 return;
