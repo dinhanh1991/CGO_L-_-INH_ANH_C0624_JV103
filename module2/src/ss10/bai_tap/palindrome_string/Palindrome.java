88a8e246b8b6fea69f484e4d4917113225a8ba93
Your Name <you@example.com>
2024-09-24 23:28:19 +0700

    nopbaiss10

---
+++ module2/src/ss10/bai_tap/palindrome_string/Palindrome.java
@@ -0,0 +1,35 @@
+package ss10.bai_tap.palindrome_string;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String originalString;
+        Stack<String> stackString = new Stack<>();
+        Queue<String> queueString = new LinkedList<>();
+        System.out.println("Enter original string");
+        Scanner scanner = new Scanner(System.in);
+        originalString = scanner.nextLine();
+        for (int i = 0; i < originalString.length(); i++) {
+            stackString.push(Character.toString(originalString.charAt(i)));
+            queueString.offer(Character.toString(originalString.charAt(i)));
+        }
+        System.out.println(stackString.pop());
+        System.out.println(queueString.poll());
+        boolean isPalindrome = true;
+        while (!stackString.isEmpty()) {
+            if (!stackString.pop().equals(queueString.poll())){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome){
+            System.out.println("The String "+"\""+originalString+"\" is Palindrome");
+        }else {
+            System.out.println("The String "+"\""+originalString+"\" is not Palindrome");
+        }
+    }
+}
