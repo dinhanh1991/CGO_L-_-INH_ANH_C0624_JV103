f96cd5fd97aeafc2dc486406acc2328508268f89
Your Name <you@example.com>
2024-09-28 20:08:50 +0700

    nopbaiss10

--- module2/src/ss9/bai_tap/crued_by_array_list/controller/ControllerCustomerList.java
+++ module2/src/ss9/bai_tap/crued_by_array_list/controller/ControllerCustomerList.java
@@ -11,6 +11,7 @@ public class ControllerCustomerList {
     public static void main(String[] args) {
         displayCustomerList();
     }
+
     public static void displayCustomerList() {
         int choice;
         do {
@@ -19,7 +20,7 @@ public class ControllerCustomerList {
                     "2.Display Customer\n" +
                     "3.Delete Customer\n" +
                     "4.Update Customer\n" +
-                    "5.Search for name\n"+
+                    "5.Search for name\n" +
                     "0.Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
@@ -42,9 +43,9 @@ public class ControllerCustomerList {
                     System.out.println("List Customer after updating :");
                     service.showCustomer();
                     break;
-                    case 5:
-                        service.lookingForCustomerByName();
-                        break;
+                case 5:
+                    service.lookingForCustomerByName();
+                    break;
                 case 0:
                     System.out.println("Goodbye!");
                     System.out.println("Exiting....");
